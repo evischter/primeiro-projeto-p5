de4ea1e36dcef543ce3ca7e8c84475910d5b63c6
evischter <134930540+evischter@users.noreply.github.com>
2023-09-21 18:37:56 -0300

    adicionando arquivos js e css

---
+++ sketch.js
@@ -0,0 +1,9 @@
+function setup() {
+    createCanvas(400, 400);
+    background("rgb(255,35,97) ");
+  }
+  
+  function draw() {
+    // console.log( mouseIsPressed)
+    if(mouseIsPressed){
+    rect(mo
